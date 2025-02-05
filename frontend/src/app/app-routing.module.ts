a6907092960c598299aba1fc8da22aaefd144ae1
Juan Augusto Haser <juanaugustohaser@gmail.com>
2025-02-05 20:43:44 -0300

    Feature: Added more routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,12 +5,34 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 
-    /*{ path: 'second-component', component: SecondComponent },
-    { path: '',   redirectTo: '/first-component', pathMatch: 'full' },*/
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
+
+    { path: 'agregarGruero', component: LoginComponent },
+    { path: 'modificarGruero', component: HomeComponent },
+    { path: 'verGrueros', component: LoginComponent },
+
+    { path: 'agregarPedidoGrua', component: HomeComponent },
+    { path: 'modificarPedidoGrua', component: LoginComponent },
+    { path: 'verPedidosDeGrua', component: HomeComponent },
+
+    { path: 'agregarVehiculo', component: HomeComponent },
+    { path: 'modificarVehiculo', component: LoginComponent },
+    { path: 'verVehiculos', component: HomeComponent },
+
+    { path: 'agregarPoliza', component: HomeComponent },
+    { path: 'modificarPoliza', component: LoginComponent },
+    { path: 'verPolizas', component: HomeComponent },
+
+    { path: 'agregarCuota', component: HomeComponent },
+    { path: 'modificarCuota', component: LoginComponent },
+    { path: 'verCuotas', component: HomeComponent },
+
+    { path: 'verReportesDeGruas', component: HomeComponent },
+    { path: 'verReportesDeVehiculos', component: LoginComponent },
+    { path: 'verReportesDePolizas', component: HomeComponent },
+
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    /*{ path: '**', component: PageNotFoundComponent }, */
   ];
 
   
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'proyecto-seminario';
+  showNavbar: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showNavbar = !event.url.includes('/login');
+      }
+    });
+  }
 }
