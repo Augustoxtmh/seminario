e12ddfaa14080812976fb28f8810d9ed8dd7a0e6
Juan Augusto Haser <juanaugustohaser@gmail.com>
2025-02-26 23:55:01 -0300

    Feature: now the program make recomendations when searching for a gruero name

--- frontend/src/app/pedidoGrua/agregar-pgrua/agregar-pgrua.component.ts
+++ frontend/src/app/pedidoGrua/agregar-pgrua/agregar-pgrua.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PGrua } from 'src/app/models/pgrua';
 import { Poliza } from 'src/app/models/poliza';
+import { GrueroService } from 'src/app/service/gruero/gruero.service';
 import { PgruaService } from 'src/app/service/pgrua/pgrua.service';
+import { VehiculoService } from 'src/app/service/vehiculo/vehiculo.service';
 
 @Component({
   selector: 'app-agregar-pgrua',
@@ -11,11 +13,14 @@ import { PgruaService } from 'src/app/service/pgrua/pgrua.service';
 })
 export class AgregarPGruaComponent {
 
+  patentesSugeridas: String[] = [];
+  grueroSugerido: String[] = [];
   formularioPoliza: FormGroup;
   polizas: Poliza[] = [];
   date: Date = new Date();
 
-  constructor(private fb: FormBuilder, private pGruaServ: PgruaService)
+  constructor(private fb: FormBuilder, private pGruaServ: PgruaService,
+      private vehiculoServ: VehiculoService, private grueroServ: GrueroService)
   {
     this.formularioPoliza = this.fb.group({
       gruero: [
@@ -50,4 +55,54 @@ export class AgregarPGruaComponent {
       console.log('Pedido creado:', res);
     });
   }
+
+  onPatenteInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const value = inputElement.value.trim();
+    
+    if (value.length > 2) {
+      this.buscarPatentes(value);
+    } else {
+      this.patentesSugeridas = [];
+    }
+  }
+  
+  setValuePatente(patente: String) {
+    this.formularioPoliza.controls['patente'].setValue(patente);
+    this.patentesSugeridas = [];
+  }  
+
+  buscarPatentes(query: string) {
+    this.vehiculoServ.getVehiculosPorPatente(query).subscribe(
+      (vehiculos: String[]) => {
+        this.patentesSugeridas = vehiculos.map(vehiculo => vehiculo);
+      }
+    );
+  }
+
+  onGrueroInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const value = inputElement.value.trim();
+  
+    if (value.length > 2) {
+      this.buscarGrueros(value);
+    } else {
+      this.grueroSugerido = [];
+    }
+  }
+  
+  buscarGrueros(value: string) {
+    this.grueroServ.getAllGrueros().subscribe(
+      (grueros) => {
+        this.grueroSugerido = grueros
+          .map(gruero => gruero.NombreGruero)
+          .filter(nombre => nombre.toLowerCase().includes(value.toLowerCase())); // Filtramos los nombres que coinciden con lo ingresado
+      }
+    );
+  }
+  
+  setValueGruero(nombre: String) {
+    this.formularioPoliza.controls['gruero'].setValue(nombre);
+    this.grueroSugerido = [];
+  }  
 }
